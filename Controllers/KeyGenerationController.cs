1cfec712c438e1020da30964e28b64b7d923f5ed
nurbeeyza <bnurmete26@gmail.com>
2024-11-28 15:29:56 +0300

    first commit

---
+++ Controllers/KeyGenerationController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Anahtar_oluşturma.Models;
+using System.IO;
+
+namespace Anahtar_oluşturma.Controllers
+{
+    public class KeyGenerationController : Controller
+    {
+        private readonly string publicKeyPath = "C:\\Anahtar\\publicKey.txt";
+        private readonly string privateKeyPath = "C:\\Anahtar\\privateKey.txt";
+        private readonly string encryptedDataPath = "C:\\Anahtar\\encryptedData.txt"; // Şifreli veriyi saklamak için yol
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateKeys()
+        {
+            var model = new KeyGenerationModel(publicKeyPath, privateKeyPath);
+            model.GenerateKeyPair();
+
+            ViewBag.Message = "Anahtar çiftleri başarıyla oluşturuldu.";
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult EncryptAndDecryptData(string data)
+        {
+            var model = new KeyGenerationModel(publicKeyPath, privateKeyPath);
+            var rsa = model.ImportKeys(publicKeyPath, privateKeyPath);
+
+            // Veriyi şifrele
+            string encData = model.EncryptData(data, rsa);
+            // Şifreli veriyi dosyaya yaz
+            System.IO.File.WriteAllText(encryptedDataPath, encData);
+
+            // Şifreli veriyi deşifre et
+            ViewBag.Enc = encData;
+            ViewBag.Dec = model.DecryptData(encData, rsa);
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult DecryptData(string encdata)
+        {
+            var model = new KeyGenerationModel(publicKeyPath, privateKeyPath);
+            var rsa = model.ImportKeys(publicKeyPath, privateKeyPath);
+
+            ViewBag.EncData = encdata;
+            ViewBag.DecData = model.DecryptData(encdata, rsa);
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ReadEncryptedData()
+        {
+            // Dosyadan şifreli veriyi oku
+            if (System.IO.File.Exists(encryptedDataPath))
+            {
+                string encDataFromFile = System.IO.File.ReadAllText(encryptedDataPath);
+                var model = new KeyGenerationModel(publicKeyPath, privateKeyPath);
+                var rsa = model.ImportKeys(publicKeyPath, privateKeyPath);
+
+                ViewBag.EncDataFromFile = encDataFromFile;
+                ViewBag.DecDataFromFile = model.DecryptData(encDataFromFile, rsa);
+            }
+            else
+            {
+                ViewBag.Message = "Şifreli veri dosyası bulunamadı.";
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Models/KeyGenerationModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Anahtar_oluşturma.Models
+{
+    public class KeyGenerationModel
+    {
+        public string PublicKeyPath { get; set; }
+        public string PrivateKeyPath { get; set; }
+
+        public KeyGenerationModel(string publicKeyPath, string privateKeyPath)
+        {
+            PublicKeyPath = publicKeyPath;
+            PrivateKeyPath = privateKeyPath;
+        }
+
+        public void GenerateKeyPair()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                File.WriteAllText(PublicKeyPath, Convert.ToBase64String(rsa.ExportRSAPublicKey()));
+                File.WriteAllText(PrivateKeyPath, Convert.ToBase64String(rsa.ExportRSAPrivateKey()));
+            }
+        }
+
+        public RSA ImportKeys(string publicKeyPath, string privateKeyPath)
+        {
+            var rsa = RSA.Create();
+
+            string base64Public = File.ReadAllText(publicKeyPath);
+
+            byte[] publicBytes = Convert.FromBase64String(base64Public);
+            string base64Private = File.ReadAllText(privateKeyPath);
+
+            byte[] privateBytes = Convert.FromBase64String(base64Private);
+
+            rsa.ImportRSAPublicKey(publicBytes, out _);
+            rsa.ImportRSAPrivateKey(privateBytes, out _);
+
+
+            return rsa;
+        }
+
+        public string EncryptData(string data, RSA rsa)
+        {
+                
+
+                byte[] DataBytes = Encoding.UTF8.GetBytes(data);
+
+                byte[] encryptedData = rsa.Encrypt(DataBytes, RSAEncryptionPadding.Pkcs1);
+
+                return Convert.ToBase64String(encryptedData);
+        }
+
+        public string DecryptData(string encryptedData, RSA rsa)
+        {
+                byte[] DataBytes = Convert.FromBase64String(encryptedData);
+
+                byte[] Data = rsa.Decrypt(DataBytes, RSAEncryptionPadding.Pkcs1);
+
+                return Encoding.UTF8.GetString(Data);
+        }
+    }
+}
